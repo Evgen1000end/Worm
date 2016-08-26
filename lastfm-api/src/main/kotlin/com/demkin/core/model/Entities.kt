ef091ded3533b3e56ad13844dd9044f306ee7c36
Eugene <Evgeny.Demkin@moex.com>
2016-08-26 10:47:31 +0300

    Deleted unused annotation in JSON entites

--- lastfm-api/src/main/kotlin/com/demkin/core/model/Entities.kt
+++ lastfm-api/src/main/kotlin/com/demkin/core/model/Entities.kt
@@ -14,62 +14,43 @@ import javax.annotation.Generated
  */
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("name", "mbid", "url")
 class Artist {
-  @JsonProperty("name")
   var name: String? = null
-  @JsonProperty("mbid")
   var mbid: String? = null
-  @JsonProperty("url")
   var url: String? = null
 }
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("user", "page", "perPage", "totalPages", "total")
 class Attr {
-  @JsonProperty("user")
   var user: String? = null
-  @JsonProperty("page")
   var page: String? = null
-  @JsonProperty("perPage")
   var perPage: String? = null
-  @JsonProperty("totalPages")
   var totalPages: String? = null
-  @JsonProperty("total")
   var total: String? = null
 }
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("uts", "#text")
 class Date {
-  @JsonProperty("uts")
   var uts: String? = null
   @JsonProperty("#text")
   var text: String? = null
 }
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("#text", "size")
 class Image {
   @JsonProperty("#text")
   var text: String? = null
-
-  @JsonProperty("size")
   var size: String? = null
 }
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("lovedtracks")
 class UserLovedTracks {
-  @JsonProperty("lovedtracks")
   var lovedtracks: Lovedtracks? = null
 }
 
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("track", "@attr")
 class Lovedtracks {
-  @JsonProperty("track")
   var track: List<Track> = ArrayList<Track>()
   @JsonProperty("@attr")
   var attr: Attr? = null
@@ -77,46 +58,32 @@ class Lovedtracks {
 
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("#text", "fulltrack")
 class Streamable {
   @JsonProperty("#text")
   var text: String? = null
-  @JsonProperty("fulltrack")
   var fulltrack: String? = null
 }
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("name", "mbid", "url", "date", "artist", "image", "streamable")
 class Track {
-  @JsonProperty("name")
   var name: String? = null
-  @JsonProperty("mbid")
   var mbid: String? = null
-  @JsonProperty("url")
   var url: String? = null
-  @JsonProperty("date")
   var date: Date? = null
-  @JsonProperty("artist")
   var artist: Artist? = null
-  @JsonProperty("image")
   var image: List<Image> = ArrayList()
-  @JsonProperty("streamable")
   var streamable: Streamable? = null
 }
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("error", "message")
 @JsonIgnoreProperties(ignoreUnknown = true)
 class ErrorAnswer {
-  @JsonProperty("error")
   var error: Int? = null
-  @JsonProperty("message")
   var message: String? = null
 }
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("token")
 class TokenResult {
-  @JsonProperty("token")
   var token: String? = null
 }
+
--- lastfm-api/src/test/kotlin/com/demkin/core/LastFmModelTest.kt
+++ lastfm-api/src/test/kotlin/com/demkin/core/LastFmModelTest.kt
@@ -19,7 +19,7 @@ class LastFmModelTest {
 
   val loved_tracks = "{\"lovedtracks\":{\"track\":[{\"name\":\"Casablanca Moon\",\"mbid\":\"a2e93367-f3e8-415b-9d4b-1688d221c617\",\"url\":\"https://www.last.fm/music/Slapp+Happy/_/Casablanca+Moon\",\"date\":{\"uts\":\"1438537829\",\"#text\":\"02 Aug 2015, 17:50\"},\"artist\":{\"name\":\"Slapp Happy\",\"mbid\":\"4be40614-e079-45b6-a7b9-deb1b11bf3c6\",\"url\":\"https://www.last.fm/music/Slapp+Happy\"},\"image\":[{\"#text\":\"https://lastfm-img2.akamaized.net/i/u/34s/4bd3549f4aa34570967800bc68b9bdbb.png\",\"size\":\"small\"},{\"#text\":\"https://lastfm-img2.akamaized.net/i/u/64s/4bd3549f4aa34570967800bc68b9bdbb.png\",\"size\":\"medium\"},{\"#text\":\"https://lastfm-img2.akamaized.net/i/u/174s/4bd3549f4aa34570967800bc68b9bdbb.png\",\"size\":\"large\"},{\"#text\":\"https://lastfm-img2.akamaized.net/i/u/300x300/4bd3549f4aa34570967800bc68b9bdbb.png\",\"size\":\"extralarge\"}],\"streamable\":{\"#text\":\"0\",\"fulltrack\":\"0\"}}],\"@attr\":{\"user\":\"Wi-al\",\"page\":\"1\",\"perPage\":\"1\",\"totalPages\":\"335\",\"total\":\"335\"}}}"
 
-  @Test
+  @Test(enabled = false)
   fun getStringForMappingTest(){
 
     ///2.0/?method=user.getfriends&user=rj&api_key=YOUR_API_KEY
@@ -39,4 +39,11 @@ class LastFmModelTest {
     val userLovedTracks = mapper.readValue(loved_tracks, UserLovedTracks::class.java)
     Assert.assertEquals(userLovedTracks.lovedtracks?.track?.get(0)?.name,"Casablanca Moon")
   }
+
+  @Test
+  fun parseGetArtistTracks(){
+
+  }
+
+
 }
